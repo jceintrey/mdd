1df5a5cc1b885625762079bb99a1545d261f49a1
Jeremie Ceintrey <jeremie.ceintrey@gmail.com>
2025-04-11 11:51:03 +0200

    front/login  - add login component with form and its css

---
+++ front/src/app/feature/auth/login/login.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { LoginRequest } from '../../../core/interfaces/loginRequest.interface';
+import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, MatIcon, MatFormFieldModule, MatInputModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+  isMobile = true;
+  public onError = false;
+
+
+  constructor(private observer: BreakpointObserver, private formBuilder: FormBuilder, private router: Router, private authService: AuthService) {
+
+    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
+      if (screenSize.matches) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    });
+  }
+
+
+
+
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      identifier: [
+        '',
+        [
+          Validators.required,
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.min(12)
+        ]
+      ]
+    });
+  }
+
+  onSubmit() {
+    const LoginRequest = this.loginForm.value as LoginRequest;
+
+    this.authService.login(LoginRequest).subscribe({
+      next: () => {
+        console.log("authService.login ok");
+        this.router.navigate(['']);
+      },
+      error: (err: any) => {
+        console.log("authService.login nok");
+        this.onError = true;
+        console.error(err);
+      }
+
+    });
+  }
+}
