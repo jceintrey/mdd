cdb06fb1a86cf7d841b630e901ae7649052c4a40
Jeremie Ceintrey <jeremie.ceintrey@gmail.com>
2025-04-14 16:53:37 +0200

    front/user-profile
     - add simple user profile view with no styles
     - subscriptions view in /me will be added later

--- front/src/app/core/interfaces/User.interface.ts
+++ front/src/app/core/interfaces/User.interface.ts
@@ -1,4 +1,8 @@
+
+
 export interface User {
+    id: number;
     username: string;
     email: string;
+
 }
--- front/src/app/feature/user/component/me/me.component.ts
+++ front/src/app/feature/user/component/me/me.component.ts
@@ -4,33 +4,53 @@ import { User } from '../../../../core/interfaces/User.interface';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../../core/services/auth.service';
 import { UserService } from '../../../../core/services/user.service';
+import { TopicService } from 'app/core/services/topic.service';
+import { Observable } from 'rxjs';
+import { Topic } from 'app/core/interfaces/topic.interface';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-me',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatDividerModule],
   templateUrl: './me.component.html',
   styleUrl: './me.component.scss'
 })
 export class MeComponent {
   user: User | null = null;
   errorMessage: string | null = null;
-
-
-  constructor(private http: HttpClient, private authService: AuthService, private userService: UserService) { }
+  topics$!: Observable<Topic[]>;
+  userForm!: FormGroup;
+  constructor(private authService: AuthService, private userService: UserService, private topicService: TopicService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
 
     this.userService.getMe().subscribe({
-      next: (data) => (this.user = data),
+      next: (data) => {
+        this.user = data
+
+        this.userForm = this.fb.group({
+          username: ['', [Validators.minLength(3)]],
+          email: ['', [Validators.email]],
+          password: ['', [Validators.minLength(3)]]
+        });
+
+        this.topics$ = this.topicService.all()
+
+      },
       error: (err) => {
         console.log("Error on Mecomponent.getMe()");
         this.errorMessage = "Error while getting infos";
       }
     })
   }
-  logout() {
-    this.authService.logout();
-    console.log("me.logout")
+
+
+  onSubmit() {
+
+    console.log("try to submit userForm");
   }
 }
