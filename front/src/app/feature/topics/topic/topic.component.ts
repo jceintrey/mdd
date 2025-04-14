02b31995766d4cc806fdd3bfc8f6ba26f93b75cf
Jeremie Ceintrey <jeremie.ceintrey@gmail.com>
2025-04-14 18:35:44 +0200

    front/topicComponent:
     - ajout du composant TopicComponent qui gère uniquement l'affichage d'une carte topic
     - TopicsComponent est le composant parent avec des TopicComponent enfants
     - ajout d'un @input dans TopicComponent pour injecter le topic depuis le parent et de deux @output pour remonter le traitement de l'évènement dans le composant parent
     - utilisation du TopicCOmponent dans meComponent pour afficher la liste des topics "souscrits"

---
+++ front/src/app/feature/topics/topic/topic.component.ts
@@ -0,0 +1,27 @@
+import { NgClass } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Topic } from 'app/core/interfaces/topic.interface';
+
+@Component({
+  selector: 'app-topic',
+  imports: [MatCardModule, NgClass],
+  templateUrl: './topic.component.html',
+  styleUrl: './topic.component.scss'
+})
+export class TopicComponent {
+  @Input() topic!: Topic;
+
+  @Output() subscribe = new EventEmitter<number>();
+  @Output() unsubscribe = new EventEmitter<number>();
+
+
+  onToggleSubscription() {
+    if (this.topic.subscribed) {
+      this.unsubscribe.emit(this.topic.id);
+    } else {
+      this.subscribe.emit(this.topic.id);
+    }
+  }
+
+}
--- front/src/app/feature/user/component/me/me.component.ts
+++ front/src/app/feature/user/component/me/me.component.ts
@@ -5,26 +5,29 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../../core/services/auth.service';
 import { UserService } from '../../../../core/services/user.service';
 import { TopicService } from 'app/core/services/topic.service';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Topic } from 'app/core/interfaces/topic.interface';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
+import { TopicComponent } from "../../../topics/topic/topic.component";
+import { SubscriptionService } from 'app/core/services/subscription.service';
 
 @Component({
   selector: 'app-me',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatDividerModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatDividerModule, TopicComponent],
   templateUrl: './me.component.html',
   styleUrl: './me.component.scss'
 })
 export class MeComponent {
+
   user: User | null = null;
   errorMessage: string | null = null;
   topics$!: Observable<Topic[]>;
   userForm!: FormGroup;
-  constructor(private authService: AuthService, private userService: UserService, private topicService: TopicService, private fb: FormBuilder) { }
+  constructor(private authService: AuthService, private userService: UserService, private topicService: TopicService, private fb: FormBuilder, private subscriptionService: SubscriptionService) { }
 
   ngOnInit(): void {
 
@@ -38,7 +41,7 @@ export class MeComponent {
           password: ['', [Validators.minLength(3)]]
         });
 
-        this.topics$ = this.topicService.all()
+        this.reloadSubscriptions();
 
       },
       error: (err) => {
@@ -53,4 +56,26 @@ export class MeComponent {
 
     console.log("try to submit userForm");
   }
+
+  reloadSubscriptions() {
+    this.topics$ = this.topicService.all().pipe(
+      map(topics => topics.filter(topic => topic.subscribed))
+    );
+  }
+
+  unsubscribe(topicId: number) {
+    console.log("me.unsuscribe from " + topicId);
+    this.subscriptionService.unsubscribe(topicId).subscribe({
+      next: () => {
+        console.log("topic.unsubscribe ok");
+        this.reloadSubscriptions();
+      },
+      error: (err: any) => {
+        console.log("topic.unsubscribe error");
+
+      }
+    });
+
+  }
+
 }
