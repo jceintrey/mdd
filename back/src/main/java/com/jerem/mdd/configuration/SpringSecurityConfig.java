b5b51751323a475f0dad2743f0d4b554977adc5f
Jeremie Ceintrey <jeremie.ceintrey@gmail.com>
2025-04-29 09:04:52 +0200

    fix/password-checking
     - fix password complexity checking in front
     - add localhost in cors config allowed origin

--- back/src/main/java/com/jerem/mdd/configuration/SpringSecurityConfig.java
+++ back/src/main/java/com/jerem/mdd/configuration/SpringSecurityConfig.java
@@ -25,8 +25,10 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * Spring Security configuration class for the application.
  *
- * This class sets up HTTP security, authentication mechanisms, and JWT integration to secure the
- * API. It leverages stateless session management and customizes the {@link SecurityFilterChain} to
+ * This class sets up HTTP security, authentication mechanisms, and JWT
+ * integration to secure the
+ * API. It leverages stateless session management and customizes the
+ * {@link SecurityFilterChain} to
  * define public and protected endpoints.
  */
 @Configuration
@@ -36,11 +38,10 @@ public class SpringSecurityConfig {
         private final UserDetailsService userDetailsService;
         private final JwtTokenProvider jwtTokenProvider;
 
-        private static final String[] AUTH_WHITELIST =
-                        {"/api/auth/login", "/api/auth/register", "/swagger-ui/**",
-                                        "/v3/api-docs/**"
+        private static final String[] AUTH_WHITELIST = { "/api/auth/login", "/api/auth/register", "/swagger-ui/**",
+                        "/v3/api-docs/**"
 
-                        };
+        };
 
         public SpringSecurityConfig(UserDetailsService userDetailsService,
                         JwtTokenProvider jwtTokenProvider) {
@@ -49,12 +50,16 @@ public class SpringSecurityConfig {
         }
 
         /**
-         * Configures the {@link SecurityFilterChain} bean, overiding default security rules applied
+         * Configures the {@link SecurityFilterChain} bean, overiding default security
+         * rules applied
          * to HTTP requests.
          * <p>
-         * This configuration: - Disables CSRF protection (may need to be adjusted for specific use
-         * cases). - Permits access to the {@link AUTH_WHITELIST} URL without authentication. -
-         * Requires authentication for all other requests. - Configures Spring Security to act as an
+         * This configuration: - Disables CSRF protection (may need to be adjusted for
+         * specific use
+         * cases). - Permits access to the {@link AUTH_WHITELIST} URL without
+         * authentication. -
+         * Requires authentication for all other requests. - Configures Spring Security
+         * to act as an
          * OAuth Ressource Server and enforce jwt authentication
          * </p>
          * 
@@ -80,10 +85,12 @@ public class SpringSecurityConfig {
         }
 
         /**
-         * This bean is requierd by Spring security and provides a {@link PasswordEncoder} to use
+         * This bean is requierd by Spring security and provides a
+         * {@link PasswordEncoder} to use
          * BCrypt for password encoding.
          * <p>
-         * BCrypt is a secure hashing algorithm used to protect user passwords in the database.
+         * BCrypt is a secure hashing algorithm used to protect user passwords in the
+         * database.
          * </p>
          * 
          * @return the {@link PasswordEncoder} bean configured with BCrypt
@@ -116,12 +123,14 @@ public class SpringSecurityConfig {
         }
 
         /**
-         * Configures the Spring Security {@link AuthenticationManager} bean, which is responsible
+         * Configures the Spring Security {@link AuthenticationManager} bean, which is
+         * responsible
          * for authenticating users. This bean is requierd by Spring security.
          * <p>
          * </p>
          * 
-         * @param http the {@link HttpSecurity} instance, required for configuring authentication.
+         * @param http            the {@link HttpSecurity} instance, required for
+         *                        configuring authentication.
          * @param passwordEncoder the {@link PasswordEncoder} used to check passwords.
          * @return the configured {@link AuthenticationManager} bean
          * @throws Exception if there is a configuration error
@@ -137,10 +146,12 @@ public class SpringSecurityConfig {
         }
 
         /**
-         * Configures global CORS settings to allow frontend requests from localhost:4200.
+         * Configures global CORS settings to allow frontend requests from
+         * localhost:4200.
          *
          * <p>
-         * Allows all paths (/**) to accept cross-origin requests from the frontend dev server with
+         * Allows all paths (/**) to accept cross-origin requests from the frontend dev
+         * server with
          * HTTP methods GET, POST, PUT, DELETE.
          * </p>
          *
@@ -153,7 +164,7 @@ public class SpringSecurityConfig {
                         @Override
                         public void addCorsMappings(CorsRegistry registry) {
                                 registry.addMapping("/**")
-                                                .allowedOrigins("http://127.0.0.1:4200")
+                                                .allowedOrigins("http://127.0.0.1:4200", "http://localhost:4200")
                                                 .allowedMethods("GET", "POST", "PUT", "DELETE");
                         }
                 };
